8b7cb8031aac83727a8b084acdaa8b6a3d2d5b07
alistair46 <aefrancis12@gmail.com>
2024-03-12 18:06:22 +0530

    double link list update

--- DSA/double_link_list.py
+++ DSA/double_link_list.py
@@ -7,3 +7,41 @@ class Node: #Represent indivisual element in an link list
 class Doubled_linked_list:
     def __init__(self):
         self.head=None  #Head  of link list
+
+#In this function set a pointer to head and increnment till end and print all the data 
+    def print(self):
+        if self.head is None:
+            print("Link list is empty")
+            return
+        itr=self.head
+        llstr=""
+        while itr:
+            llstr+=str(itr.data)+'-->'+'<--'
+            itr=itr.next
+        print(llstr)
+
+# to append elenment in double link list        
+    def append(self,data):
+        if self.head is None: # check if there is elenment present in Double linklist
+            new_node=Node(data) #create a new node if double link list is empty 
+            new_node.prev=None  # set prev of new node ie head to null
+            self.head=new_node  # update the status of head pointer , here the new node becomes the head
+
+#condition when the is elenment present in list so we need to append after the elenment ,ie end of list
+        else : 
+            new_node=Node(data) #create a new node if double link list is empty
+            cur=self.head       #temp variable that goeas from head to end of list
+            while cur.next:     #Check if the next to node is Null , if yes then we are at the end of list
+                cur=cur.next    #increnment temp variable
+            cur.next=new_node
+            new_node.prev=cur   #update the new node prev to cur as it contain the last elenment address in it
+            new_node.next=None  #set next of new node to Null
+
+if __name__=='__main__':
+    a=Doubled_linked_list()
+    a.append(12)
+    a.append(13)
+    a.append(14)
+    a.append(16)
+    a.append(17)
+    a.print()
